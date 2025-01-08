190c0878f39afed3fc77d2e1739a62cf07acaf0b
Teka-a <katya-osipova-1999@mail.ru>
2025-01-08 23:45:34 +0300

    test added

--- Set_1/tests/convertions_tests.cpp
+++ Set_1/tests/convertions_tests.cpp
@@ -10,3 +10,15 @@ TEST(hex_to_base64, BasicAssertions)
     std::string base64_correct {"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"};
     EXPECT_EQ(base64, base64_correct);
 }
+
+TEST(base64_to_hex, BasicAssertions)
+{
+    std::string base64 {"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"};
+    std::string hex;
+    base64_to_hex(base64, hex);
+
+    std::string hex_correct {"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"};
+
+    
+    EXPECT_EQ(hex, hex_correct);
+}
