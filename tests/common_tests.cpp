649aa35ae795cd8187aaa8e01188bec7c05bd909
Teka-a <katya-osipova-1999@mail.ru>
2025-01-13 08:44:15 +0300

    some tests added

--- tests/common_tests.cpp
+++ tests/common_tests.cpp
@@ -12,3 +12,58 @@ TEST(XOR_hex_strs, BasicAssertions)
 
     EXPECT_EQ(res, c);
 }
+
+
+TEST(padding_left, BasicAssertions)
+{
+    std::string a {"1111111111"};
+    std::string c {"00000000001111111111"};
+    
+    padding(a, '0', 20, 0);
+
+    EXPECT_EQ(a, c);
+}
+
+
+TEST(padding_right, BasicAssertions)
+{
+    std::string a {"1111111111"};
+    std::string c {"11111111110000000000"};
+    
+    padding(a, '0', 20, 1);
+
+    EXPECT_EQ(a, c);
+}
+
+
+TEST(padding_bytes, BasicAssertions)
+{
+    bytes test_vec {0x89, 0x56, 0x80, 0x89, 0x56, 0x80, 0x89, 0x56, 0x80, 0x89, 0x56, 0x80, 0x89, 0x56, 0x80};
+    bytes res_vec {0x00, 0x89, 0x56, 0x80, 0x89, 0x56, 0x80, 0x89, 0x56, 0x80, 0x89, 0x56, 0x80, 0x89, 0x56, 0x80};
+
+    padding_bytes(test_vec, 16);
+
+    EXPECT_EQ(test_vec, res_vec);
+}
+
+
+TEST(insert_symbol, BasicAssertions)
+{
+    std::string a {"1111111111"};
+    std::string c {"00000000001111111111"};
+    
+    insert_symbol(a, '0', 10);
+
+    EXPECT_EQ(a, c);
+}
+
+
+TEST(slice, BasicAssertions)
+{
+    bytes test_vec {0x89, 0x56, 0x80, 0x89, 0x56, 0x80, 0x89, 0x56, 0x80, 0x89, 0x56, 0x80, 0x89, 0x56, 0x80};
+    bytes sliced_vec = slice(test_vec, 3, 5);
+    bytes res_vec {0x89, 0x56, 0x80, 0x89, 0x56};
+
+
+    EXPECT_EQ(sliced_vec, res_vec);
+}
